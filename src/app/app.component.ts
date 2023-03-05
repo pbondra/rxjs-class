030310cf9a7c3d4c6359af12e9b51a49925bf320
pbondra <118773582+pbondra@users.noreply.github.com>
2023-03-05 15:43:50 -0800

    Piping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, VERSION } from '@angular/core';
-import { of, from } from 'rxjs';
+import { of, from, map, tap, take } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -12,19 +12,30 @@ export class AppComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    of(2, 4, 6, 8).subscribe(item => console.log(item));
+    // of(2, 4, 6, 8).subscribe(item => console.log(item));
 
-    from([20, 15, 10, 5]).subscribe({
+    from([20, 15, 10, 5]).pipe(
+      tap(item => console.log(`emitted item ... ${item}`)),
+      map(item => item * 2),
+      map(item => item - 10),
+      map(item => {
+        if (item == 0) {
+          throw new Error('zero detected');
+        }
+        return item;
+      }),
+      take(3)
+    ).subscribe({
       next: (item) => console.log(`resulting item .. ${item}`),
       error: (err) => console.error(`error occurred ${err}`),
       complete: () => console.log('complete'),
     });
 
-    of('Apple1', 'Apple2', 'Apple3').subscribe({
-      next: (apple) => console.log(`Apple emitted ${apple}`),
-      error: (err) => console.error(`Error occurred: ${err}`),
-      complete: () => console.log(`No more apples, go home`)
-    })
+    // of('Apple1', 'Apple2', 'Apple3').subscribe({
+    //   next: (apple) => console.log(`Apple emitted ${apple}`),
+    //   error: (err) => console.error(`Error occurred: ${err}`),
+    //   complete: () => console.log(`No more apples, go home`)
+    // })
   }
 
 }
